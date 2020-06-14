0cd31edcecfc128f37ab4b5407519243ba965d30
Anwesh43 <anweshthecool0@gmail.com>
2020-06-15 02:02:04 +0530

    adding AlterBlockCreator component

--- src/AlterBlockCreator.js
+++ src/AlterBlockCreator.js
@@ -9,3 +9,12 @@ const ABCPresentational = ({w, h, scale, onClick}) => {
       {[0, 1, 2].map(i => <Block style = {getBlockStyle(i)}/>)}
     </div>)
 }
+
+const AlterBlockCreator = (props) => {
+    const {scale, start} = useAnimatedScale(0.02 / 3, 20)
+    const {w, h} = useDimension()
+    return <ABCPresentational w = {w} h = {h} scale = {scale} onClick = {start}>
+    </ABCPresentational>
+}
+
+export default AlterBlockCreator
